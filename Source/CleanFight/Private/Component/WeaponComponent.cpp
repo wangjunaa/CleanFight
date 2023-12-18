1fb11e9658b7a0b160a027bffa66a6c3a9235754
2937574435 <2937574435@qq.com>
2023-12-18 14:13:26 +0800

    Modify

---
+++ Source/CleanFight/Private/Component/WeaponComponent.cpp
@@ -0,0 +1,25 @@
+
+
+#include "Component/WeaponComponent.h"
+
+UWeaponComponent::UWeaponComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
+void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
---
+++ Source/CleanFight/Public/Component/WeaponComponent.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeaponComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CLEANFIGHT_API UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UWeaponComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
